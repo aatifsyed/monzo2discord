252dbfdf3bd1ce17b692a93170b59b0f45394650
Aatif Syed <aatif.syed@metaswitch.com>
2020-12-07 04:26:56 +0000

    Add a statefile

--- HttpExample/__init__.py
+++ HttpExample/__init__.py
@@ -1,23 +1,55 @@
 import json
 import logging
-import requests
+import time
+from typing import Any, Optional
 
 import azure.functions as func
+import requests
+
+
+def post_transaction_to_discord(url: str, transaction_json: dict):
+    amount = int(transaction_json["data"]["amount"])
+    currency = transaction_json["data"]["currency"]
+    merchant = transaction_json["data"]["merchant"]["name"]
+    message = f"""💸 New transaction:
+    💷 {amount/100}{currency} ({"📉" if amount < 0 else "📈"})
+    📌 {merchant}"""
+
+    response = requests.post(url=url, json={"content": message})
+
+    logging.info(
+        f""""Discord:,
+    {response.status_code},
+    {response.reason},
+    {response.content}"""
+    )
+
+
+def post_monzo_balance_to_discord(
+    url: str,
+    statein: func.InputStream,
+    stateout: func.Out[func.InputStream],
+):
+    pass
 
 
 def main(
     request: func.HttpRequest,
-    discordjson: func.InputStream,
-    monzojson: func.InputStream,
+    discordfile: func.InputStream,
+    monzofile: func.InputStream,
+    statein: func.InputStream,
+    stateout: func.Out[func.InputStream],
 ) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
+    logging.info("Received a request")
 
-    logging.info(monzojson := json.load(monzojson))
-    logging.info(discordjson := json.load(discordjson))
+    logging.info(transaction_json := request.get_json())
+    logging.info(monzofile := json.load(monzofile))
+    logging.info(discordfile := json.load(discordfile))
 
-    requests.post(url=discordjson["url"], json={"content": "foo"})
+    # post_transaction_to_discord(
+    #     url=discordfile["url"], transaction_json=transaction_json
+    # )
 
     return func.HttpResponse(
-        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
         status_code=200,
     )
